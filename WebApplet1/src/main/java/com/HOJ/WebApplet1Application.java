8ff4c05083edd4f2e8d76ba1cfdfef8b7c575a47
Zoe Mitchell <mitchellzh101@gmail.com>
2018-10-13 12:15:47 -0500

    comit1 linkng github  SOJ

---
+++ WebApplet1/src/main/java/com/HOJ/WebApplet1Application.java
@@ -0,0 +1,12 @@
+package com.HOJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplet1Application.class, args);
+	}
+}
---
+++ WebApplet1/src/test/java/com/HOJ/WebApplet1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.HOJ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebApplet1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
